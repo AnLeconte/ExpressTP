1febdc293494833e368e69f692aeadc9d5e7a391
Antoine Leconte <antoine.leconte9462@gmail.com>
2024-04-25 14:15:08 +0200

    fix save json car on est des dog ici

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -35,9 +35,44 @@ router.get('/livres/:id', (req, res) => {
     });
 });
 
+const fs = require('fs');
+const path = require('path');
+
+const livresJSONPath = path.join(__dirname, '..', 'data', 'livres.json');
+
 router.post('/ajout-livre', (req, res) => {
-    res.send('Ajout d\'un nouveau livre');
+    const { titre, auteur, annee } = req.body;
+
+    fs.readFile(livresJSONPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erreur interne du serveur');
+            return;
+        }
+
+        const livres = JSON.parse(data);
+
+        const nouveauLivre = {
+            id: livres.length + 1,
+            titre: titre,
+            auteur: auteur,
+            annee: annee
+        };
+        livres.push(nouveauLivre);
+
+        const nouveauLivreJSON = JSON.stringify(livres, null, 2);
+
+        fs.writeFile(livresJSONPath, nouveauLivreJSON, 'utf8', (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Erreur interne du serveur');
+                return;
+            }
+            res.status(201).send('Livre ajouté avec succès');
+        });
+    });
 });
+
 
 router.get('/recherche-livre/auteur/:auteur', (req, res) => {
     const auteur = req.params.auteur;
